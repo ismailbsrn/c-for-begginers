060cf2fbe4179db20274d5102f3a7a8dd92f3ad3
İsmail Başaran <bsrn.ismail@gmail.com>
2024-03-18 00:33:02 +0300

    q1

---
+++ code/q1.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+  int i, a, remainder, digit;
+  printf("enter: ");
+  scanf("%d", &i);
+  a = 1000000000;
+
+  for (int c = 1; c < 11; c++)
+  {
+    remainder = i % a;
+    digit = i / a;
+    printf("%d\n", digit);
+    i = remainder;
+    a /= 10;
+  }
+
+  return 0;
+}
